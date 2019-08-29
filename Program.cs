787d1c77808b633e7a43012a1c5bf2f35a0ab2b5
Ahmed Elkalaf <kalaf1997@yahoo.com>
2019-08-29 03:47:13 +0200

    commenting code

--- Program.cs
+++ Program.cs
@@ -72,7 +72,7 @@ public class Program
 	
 	private static PointF intersectionOf3Circles(PointF[] IC0C1,PointF[] IC0C2,PointF[] IC1C2,float radius0,float radius1,float radius2)
 	{
-		//Select between intersection point 1 and intersection point2;
+		// Select between intersection point 1 and intersection point2 for each two circles using bit manpulation technique.
 		int best_premiutation = 0;
 		float min = float.MaxValue;
 		for(int i=0;i<8;i++)
@@ -96,13 +96,24 @@ public class Program
 	
 	public static bool getUserLocation(PointF center0,float radius0,PointF center1, float radius1,PointF center2, float radius2,out PointF userLocation)
 	{
+		// Each array represent the 2 intersecting points between 2 circles.
+		
+		// Intersecting point between circle0  and circle1.
 		PointF[] IC0C1 = new PointF[2];
+		
+		// Intersecting point between circle0  and circle2.
 		PointF[] IC0C2 = new PointF[2];
+		
+		// Intersecting point between circle1  and circle2.
 		PointF[] IC1C2 = new PointF[2];
-		//The Number of intersecion points
+		
+		// Where ni is the number of intersecting points
 		int n1 = intersectionOf2Circles(center0,radius0,center1,radius1,out IC0C1[0],out IC0C1[1]);
 		int n2 = intersectionOf2Circles(center0,radius0,center2,radius2,out IC0C2[0],out IC0C2[1]);
 		int n3 = intersectionOf2Circles(center1,radius1,center2,radius2,out IC1C2[0],out IC1C2[1]);
+		
+		// If ni == 0 this means there is no intersecting points between 2 circles as there is an error happened while calculating the distance.
+		// Fixing error should be handled later.
 		if(n1 == 0 || n2 == 0 || n3 == 0)
 		{
 			userLocation = new PointF(float.NaN, float.NaN);
@@ -114,12 +125,22 @@ public class Program
 	
 	public static void Main()
 	{
+		// Define the Three wifi locations
 		PointF accessPoint0 = new PointF(45,5),accessPoint1 = new PointF(15,10.54f),accessPoint2 = new PointF(7,7.5f);
-		PointF userLocation = new PointF(12,3.34f);
-		float distance0 = GetDistance(userLocation,accessPoint0),distance1 = GetDistance(userLocation,accessPoint1),distance2 = GetDistance(userLocation,accessPoint2);
-		Console.WriteLine("Current user location: "+userLocation.X+","+userLocation.Y);
+		// The Actual location of the user
+		PointF actualUserLocation = new PointF(12,3.34f);
+		
+		// Calculating the distance between the user and the three access points
+		float distance0 = GetDistance(actualUserLocation,accessPoint0);
+		float distance1 = GetDistance(actualUserLocation,accessPoint1);
+		float distance2 = GetDistance(actualUserLocation,accessPoint2);
+		
+		Console.WriteLine("Current user location: "+actualUserLocation.X+","+actualUserLocation.Y);
+		
+		// Predict the user location using the position of the access points and the distances between them and the user.
 		PointF predictedUserLocation;
 		bool canSpecifyLocation = getUserLocation(accessPoint0,distance0,accessPoint1,distance1,accessPoint2,distance2,out predictedUserLocation);
+		
 		if(canSpecifyLocation)
 			Console.WriteLine("Predicted user location: "+predictedUserLocation.X+","+predictedUserLocation.Y);
 		else
